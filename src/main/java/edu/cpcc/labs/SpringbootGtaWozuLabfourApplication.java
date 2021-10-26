c443946bb3c5faf8a4d4df7fe16a1dbc11901424
plmil <plmil@LAPTOP-I1RCR88U.mshome.net>
2021-10-26 19:07:54 -0400

    initial release

---
+++ src/main/java/edu/cpcc/labs/SpringbootGtaWozuLabfourApplication.java
@@ -0,0 +1,19 @@
+package edu.cpcc.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGtaWozuLabfourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGtaWozuLabfourApplication.class, args);
+	}
+/* don't tread on me
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+}
